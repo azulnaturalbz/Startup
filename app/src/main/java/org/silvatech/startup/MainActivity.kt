746f36fa0eec2bbfc3507df2dd643092b39c5278
csilva <csilva@silvatech.org>
2018-09-09 12:42:07 -0600

    Created a simple age calculator using concept of minus year of birth minus current year.

--- app/src/main/java/org/silvatech/startup/MainActivity.kt
+++ app/src/main/java/org/silvatech/startup/MainActivity.kt
@@ -2,7 +2,9 @@ package org.silvatech.startup
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +12,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvDisplay.text="Welcome to Kotlin"
+//        tvDisplay.text="Welcome to Kotlin"
+//        getAge.setOnClickListener{
+//            // action will happen when clicke on
+//            val calendar = Calendar.getInstance()
+//            val year:Int = calendar.get(Calendar.YEAR)
+//            var dob:Int  = ageInput.text.toString().toInt()
+//            val myAge = year - dob
+//            ageDisplay.text = "Your age in years is $myAge"
+//        }
+
+    }
+
+    fun getAge(view:View){
+        val calendar = Calendar.getInstance()
+        val year:Int = calendar.get(Calendar.YEAR)
+
+        if (ageInput.toString().isNullOrBlank() !== true || ageInput.toString().isNullOrEmpty() !== true){
+            val dob:Int  = ageInput.text.toString().toInt()
+            if (dob > 0) {
+                val myAge = year - dob
+                ageDisplay.text = "Your age in years is $myAge"
+            } else {
+                ageDisplay.text = "Kindly enter a number greater than zero please"
+                return
+            }
+        }else {
+            ageDisplay.text = "Invalid Input try again"
+            return
+        }
+//        val myAge = year - dob
+//        ageDisplay.text = "Your age in years is $myAge"
+
+
     }
 }
