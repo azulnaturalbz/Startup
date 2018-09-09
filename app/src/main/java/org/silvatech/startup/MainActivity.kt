9704074aa3d2820ad46e790b144253f77b16f84c
csilva <csilva@silvatech.org>
2018-09-09 12:44:36 -0600

    Cleared existing lines of code for clean slate.

--- app/src/main/java/org/silvatech/startup/MainActivity.kt
+++ app/src/main/java/org/silvatech/startup/MainActivity.kt
@@ -12,38 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        tvDisplay.text="Welcome to Kotlin"
-//        getAge.setOnClickListener{
-//            // action will happen when clicke on
-//            val calendar = Calendar.getInstance()
-//            val year:Int = calendar.get(Calendar.YEAR)
-//            var dob:Int  = ageInput.text.toString().toInt()
-//            val myAge = year - dob
-//            ageDisplay.text = "Your age in years is $myAge"
-//        }
 
     }
 
-    fun getAge(view:View){
-        val calendar = Calendar.getInstance()
-        val year:Int = calendar.get(Calendar.YEAR)
-
-        if (ageInput.toString().isNullOrBlank() !== true || ageInput.toString().isNullOrEmpty() !== true){
-            val dob:Int  = ageInput.text.toString().toInt()
-            if (dob > 0) {
-                val myAge = year - dob
-                ageDisplay.text = "Your age in years is $myAge"
-            } else {
-                ageDisplay.text = "Kindly enter a number greater than zero please"
-                return
-            }
-        }else {
-            ageDisplay.text = "Invalid Input try again"
-            return
-        }
-//        val myAge = year - dob
-//        ageDisplay.text = "Your age in years is $myAge"
-
-
-    }
 }
